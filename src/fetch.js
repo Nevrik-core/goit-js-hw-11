652100e0239f73db4e106d22a9d0beaca0a9dc00
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-23 23:30:20 +0300

    made backend with pagination

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,41 @@
 
-const base = "https://restcountries.com/v3.1/";
-const endpoint = 'name/';
 
-export const fetchCountries = function (name) {
+export default class SearchApiServise {
+
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
     
-    const response = fetch(`${base}${endpoint}${name}?fields=name,capital,population,flags,flag,languages`)
-        .then(response => response.json());
-    return response;
-};
+    fetchPixabay() {
+    console.log(this);
+    const base = "https://pixabay.com/api/";
+    const imageType = 'photo';
+    const orientation = 'horizontal';
+    const safesearch = true;
+    const key = '29444128-664601f5e312d32a4879ca112';
+
+
+    const url = `${base}?key=${key}&q=${this.searchQuery}&$image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${this.page}&per_page=40`)
+            fetch(url)
+                .then(response => response.json()
+                    .then(data => {
+                        this.page += 1;
+                    }));
+   
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+    
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+    
+    
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import './css/styles.scss';
-// import photoCard from "./templates/cards.hbs";
-import {fetch} from './fetch';
+import photoCard from "./templates/cards.hbs";
+import SearchApiServise from './fetch';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const start = {};
+const searchApiService = new SearchApiServise();
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  searchForm: document.querySelector('.search-form'),
+  loadMoreBtn: document.querySelector('[data-load="load-more"]')
+};
+// console.log(refs.loadMoreBtn);
+
+ 
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+   searchApiService.query = e.currentTarget.elements.search.value;
+  // console.log(searchQuery);
+  searchApiService.resetPage();
+  searchApiService.fetchPixabay();
+  
+}
+
+function onLoadMore(e) {
+  searchApiService.fetchPixabay(); 
+}
